58e3eab0c9fc9c6a5a97a7da41db6a0a8881a8a3
Paul Haas <ge49yiy@mytum.de>
2020-12-06 13:59:40 +0100

    Einlesen von Gasoline Hinzugefügt

---
+++ EinlesenderDaten.R
@@ -0,0 +1,9 @@
+library(fs)
+(MinW_text <- path_home("Documents", "R", "RSeminar", "Mineralwasser.txt"))
+  #Path wird zur Datei kreiert
+Mineralwasser <- read.table(MinW_text, header = TRUE, sep = ";")
+# Textdatei wird eingelesen
+(MinWRoh_text <- path_home("Documents", "R", "RSeminar", "Mineralwasser_Rohdaten.txt"))
+MineralwasserRohdaten <- read.table(MinWRoh_text,header = TRUE, sep = ";")
+#Gasoline data holen
+data("gasoline", package = "pls")
