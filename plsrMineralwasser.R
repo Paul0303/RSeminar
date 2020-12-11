ce0510035abc664bc345a8119024f94bda45e9a4
Paul Haas <ge49yiy@mytum.de>
2020-12-11 11:56:46 +0100

    added irgendwas

---
+++ plsrMineralwasser.R
@@ -0,0 +1,9 @@
+mineralwasser.pls <- plsr(Natrium ~ ., data = Mineralwasser, validation = "LOO",
+                          ncomp = 10)
+par(mfrow = c(1, 2))
+plot(mineralwasser.pls, "validation", estimate = "CV")
+par(pty = "s")
+plot(mineralwasser.pls, "prediction", ncomp = 3)
+abline(0, 1, col = "gray")
+plot(mineralwasser.pls, "loading", comps = 1:3, legendpos = "topleft",
+     lty = 1, col = 1:3)
