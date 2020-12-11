92669294056ca602f3cc5a64310b96df6e7ae7ef
Paul Haas <ge49yiy@mytum.de>
2020-12-11 11:58:08 +0100

    added comments

--- plsrMineralwasser.R
+++ plsrMineralwasser.R
@@ -5,5 +5,5 @@ plot(mineralwasser.pls, "validation", estimate = "CV")
 par(pty = "s")
 plot(mineralwasser.pls, "prediction", ncomp = 3)
 abline(0, 1, col = "gray")
-plot(mineralwasser.pls, "loading", comps = 1:3, legendpos = "topleft",
-     lty = 1, col = 1:3)
+#plot(mineralwasser.pls, "loading", comps = 1:3, legendpos = "topleft",
+#     lty = 1, col = 1:3)
