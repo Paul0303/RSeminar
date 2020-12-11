1e98e2d2ba4251887b19e1a950923a7613220200
Paul Haas <ge49yiy@mytum.de>
2020-12-11 12:02:11 +0100

    Element geändert und ncomp erhöht

--- JackknifeaufMineralwasser.R
+++ JackknifeaufMineralwasser.R
@@ -1,9 +1,9 @@
 wavelengths <- seq(0, 149, by = 1)
-mineralwasser.pls <- plsr(Hydrogencarbonat ~ ., data = Mineralwasser, validation = "LOO",
-                             ncomp = 2,
+mineralwasser.pls <- plsr(Nitrat ~ ., data = Mineralwasser, validation = "LOO",
+                             ncomp = 5,
                              jackknife = TRUE)
 n <- length(Mineralwasser$Natrium)
-b.oob <- mineralwasser.pls$validation$coefficients[, , 2, ]
+b.oob <- mineralwasser.pls$validation$coefficients[, , 5, ]
 bias.est <- (n-1)*(rowMeans(b.oob)-coef(mineralwasser.pls))
 plot(wavelengths, bias.est, xlab = "wavelength", ylab = "bias",
      type = "h", main = "Jackknife bias estimates",
