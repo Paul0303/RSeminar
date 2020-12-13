07ca0bc5bda6591bebdf0d570a852329b95c065d
Paul Haas <ge49yiy@mytum.de>
2020-12-13 10:53:09 +0100

    parameter wavel. umbenannt in MinGehalt

--- JackknifeaufMineralwasser.R
+++ JackknifeaufMineralwasser.R
@@ -1,13 +1,13 @@
-wavelengths <- seq(0, 149, by = 1)
-mineralwasser.pls <- plsr(Nitrat ~ ., data = Mineralwasser, validation = "LOO",
-                             ncomp = 5,
+MinGehalt <- seq(0, 149, by = 1)
+mineralwasser.pls <- plsr(Natrium ~., data = Mineralwasser, validation = "LOO",
+                             ncomp = 2,
                              jackknife = TRUE)
 n <- length(Mineralwasser$Natrium)
-b.oob <- mineralwasser.pls$validation$coefficients[, , 5, ]
+b.oob <- mineralwasser.pls$validation$coefficients[, , 2, ]
 bias.est <- (n-1)*(rowMeans(b.oob)-coef(mineralwasser.pls))
-plot(wavelengths, bias.est, xlab = "wavelength", ylab = "bias",
+plot(MinGehalt, bias.est, xlab = "Mingehalt (mg)", ylab = "bias",
      type = "h", main = "Jackknife bias estimates",
      col = "gray")
 
 var.est <- var.jack(mineralwasser.pls)
-lines(wavelengths, var.est, col = "red")
+lines(MinGehalt, var.est, col = "red")
