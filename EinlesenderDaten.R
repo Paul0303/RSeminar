c51810cb1fffd3729ac592ff0ac834284c04029d
Paul Haas <ge49yiy@mytum.de>
2020-12-13 11:57:52 +0100

    tidyverse hinzugefügt

--- EinlesenderDaten.R
+++ EinlesenderDaten.R
@@ -1,5 +1,6 @@
 library(pls)
 library(fs)
+library(tidyverse)
 (MinW_text <- path_home("Documents", "R", "RSeminar", "Mineralwasser.txt"))
   #Path wird zur Datei kreiert
 Mineralwasser <- read.table(MinW_text, header = TRUE, sep = ";")
