2e72b47e381bcbcc0d2589981d2239b984104e0b
Paul Haas <ge49yiy@mytum.de>
2020-12-13 12:35:42 +0100

    potentielles ggplot hinzugefügt was nicht funktioniert

--- Mineralwasserplot.R
+++ Mineralwasserplot.R
@@ -3,3 +3,7 @@ mineraldaten <- select(Mineralwasser, Natrium,Kalium, Magnesium, Calcium, Mangan
                        Chlorid, Sulfat, Hydrogencarbonat, Nitrat, Nitrit, Fluorid)
 matplot(Waesser, mineraldaten, type = "l",
         lty = 1, xlab = "Wassersorten", ylab = "Mineraliengehalt (mg)")
+#Mineralwasser %>%
+#  ggplot(aes(x = Wasser, y = Minearlien))+
+# geom_bar()
+#ggplot(data = mineraldaten, aes(x = Wasser, y = "Minearlien (mg)"), geom_smooth())
