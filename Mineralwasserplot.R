c35377ebbdd71c3753b6c97b55f49b830bcf9520
Paul Haas <ge49yiy@mytum.de>
2020-12-14 09:50:09 +0100

    mineraldaten wurden in datenholen verschoben

--- Mineralwasserplot.R
+++ Mineralwasserplot.R
@@ -1,6 +1,4 @@
 Waesser <- seq(1, 134,by = 1)
-mineraldaten <- select(Mineralwasser, Natrium,Kalium, Magnesium, Calcium, Mangan, 
-                       Chlorid, Sulfat, Hydrogencarbonat, Nitrat, Nitrit, Fluorid)
 matplot(Waesser, mineraldaten, type = "l",
         lty = 1, xlab = "Wassersorten", ylab = "Mineraliengehalt (mg)")
 #Mineralwasser %>%
