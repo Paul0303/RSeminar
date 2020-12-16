748976ea08fbe5ab43b07d97be5672cb0ca723b3
Paul Haas <ge49yiy@mytum.de>
2020-12-16 14:34:17 +0100

    mit ncomp rumgespielt

--- JackknifeaufMineralwasser.R
+++ JackknifeaufMineralwasser.R
@@ -1,11 +1,13 @@
-MinGehalt <- seq(1, 133, by = 1)
-mineralwasser.pls <- plsr(X1 ~.,data = mineraldatenT, validation = "LOO",
-                             ncomp = 5,
+MinGehalt <- 
+#c("Natrium", "Kalium", "Magnesium", "Calcium", "Mangan", "Chlorid", "Sulfat", "Hydrogencarbonat", "Nitrat", "Nitrit", "Fluorid")
+seq(1, 11, by = 1)
+mineralwasser.pls <- plsr(Gesamt.Mineralieninmg.Liter ~.,data = mineraldaten, validation = "LOO",
+                             ncomp = 2,
                              jackknife = TRUE)
 n <- length(Mineralwasser$Natrium)
-b.oob <- mineralwasser.pls$validation$coefficients[, , 5, ]
+b.oob <- mineralwasser.pls$validation$coefficients[, , 2, ]
 bias.est <- (n-1)*(rowMeans(b.oob)-coef(mineralwasser.pls))
-plot(MinGehalt, bias.est, xlab = "Mingehalt (mg)", ylab = "bias",
+plot(MinGehalt, bias.est, xlab = "Mineralien", ylab = "bias",
      type = "l", main = "Jackknife bias estimates",
      col = "gray")
 
