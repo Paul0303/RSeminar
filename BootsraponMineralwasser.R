1451c4682803af8c65c7367567446f902c57fb32
Paul Haas <ge49yiy@mytum.de>
2020-12-16 14:33:50 +0100

    mineraldatenT in mineraldaten geändert

--- BootsraponMineralwasser.R
+++ BootsraponMineralwasser.R
@@ -1,5 +1,5 @@
 B <- 500
-nmin <- nrow(mineraldatenT)
+nmin <- nrow(mineraldaten)
 boot.indices <-
   matrix(sample(1:nmin, nmin*B, replace = TRUE), ncol = B)
 # c <- sort(boot.indices[, 1])[1:20]
@@ -7,28 +7,28 @@ boot.indices <-
 npc <- 6
 predictions <- array(NA, c(nmin, npc, B))
 for (i in 1:B) {
-  min.bootpcr <- pcr(X1 ~., data = mineraldatenT,
+  min.bootpcr <- pcr(Gesamt.Mineralieninmg.Liter ~., data = mineraldaten,
                      ncomp = npc, subset = boot.indices[, i])
   oobs <- (1:nmin)[-boot.indices[, i]]
   predictions[oobs, , i] <-
     predict(min.bootpcr, 
-            newdata = mineraldatenT[oobs, ])[, 1, ]
+            newdata = mineraldaten[oobs, ])[, 1, ]
   
 }
-diffs <- sweep(predictions, 1, mineraldatenT$X1)
+diffs <- sweep(predictions, 1, mineraldaten$Gesamt.Mineralieninmg.Liter)
 sqerrors <- apply(diffs^2, c(1, 2),mean, na.rm = TRUE)
 sqrt(colMeans(sqerrors))
 
 
-min.pcr <- pcr(X1 ~ ., data = mineraldatenT, ncomp = npc)
+min.pcr <- pcr(Gesamt.Mineralieninmg.Liter ~ ., data = mineraldaten, ncomp = npc)
 RMSEP(min.pcr, intercept = FALSE)
 error.632 <- .368 * colMeans(min.pcr$residuals^2) + .632 * colMeans(sqerrors)
 sqrt(error.632)
 
 
-min.pcr.cv <- pcr(X1 ~ ., data = mineraldatenT, ncomp = npc, 
+min.pcr.cv <- pcr(Gesamt.Mineralieninmg.Liter ~ ., data = mineraldaten, ncomp = npc, 
                   validation = "CV")
-min.pcr.loo <- pcr(X1 ~ ., data = mineraldatenT, ncomp = npc, 
+min.pcr.loo <- pcr(Gesamt.Mineralieninmg.Liter ~ ., data = mineraldaten, ncomp = npc, 
                   validation = "LOO")
 bp <- barplot(sqrt(error.632),
               ylim = c(0, 80), col = "peachpuff")
