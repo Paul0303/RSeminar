3aecac306e47f533dabcb5b2ac96b46f98aa8ea6
Paul Haas <ge49yiy@mytum.de>
2020-12-17 08:44:12 +0100

    pcr hinzugefügt

--- plsrMineralwasser.R
+++ plsrMineralwasser.R
@@ -1,5 +1,5 @@
-mineralwasser.pls <- plsr(Natrium ~ ., data = Mineralwasser, validation = "LOO",
-                          ncomp = 10)
+mineralwasser.pls <- plsr(Gesamt.Mineralieninmg.Liter ~ ., data = mineraldaten, validation = "LOO",
+                          ncomp = 3)
 par(mfrow = c(1, 2))
 plot(mineralwasser.pls, "validation", estimate = "CV")
 par(pty = "s")
@@ -7,3 +7,10 @@ plot(mineralwasser.pls, "prediction", ncomp = 3)
 abline(0, 1, col = "gray")
 #plot(mineralwasser.pls, "loading", comps = 1:3, legendpos = "topleft",
 #     lty = 1, col = 1:3)
+
+mineralien <- seq(1, 11, by = 1)
+mineralwasser.pcr <- pcr(Gesamt.Mineralieninmg.Liter ~., data = mineraldaten,
+                         ncomp = 5)
+plot(mineralien, coef(mineralwasser.pcr), type = "l",
+     xlab = "Mineralien", ylab = "Regression coefficient")
+abline(h = 0, col = "gray")
