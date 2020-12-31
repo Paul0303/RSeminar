c502cb697a0093b79b8cc06972d1dd9d7ae7af12
Paul Haas <ge49yiy@mytum.de>
2020-12-31 09:30:32 +0100

    funktioniert jetzt

--- ConfidenceIntervals.R
+++ ConfidenceIntervals.R
@@ -7,7 +7,7 @@ npc <- 4
 min.pcr <- pcr(Gesamt.Mineralieninmg.Liter ~., data = mineraldaten, ncomp = npc)
 coefs <- matrix(0, ncol(mineraldaten)-1, B)
 for (i in 1:B) {
-min.pcr <- pcr(Gesamt.Mineralieninmg.Liter ~., data = mineraldaten,
+min.bootpcr <- pcr(Gesamt.Mineralieninmg.Liter ~., data = mineraldaten,
                ncomp = npc, subset = boot.indices[, i])
 coefs[, i] <- c(coef(min.bootpcr))
 }
@@ -15,7 +15,17 @@ coefs[, i] <- c(coef(min.bootpcr))
 matplot(mineralien, coefs, type = "n",
         ylab = "Coefficients", xlab = "Mineralien")
 abline(h = 0, col = "gray")
-polygon(c(mineralien), rev(mineralien),
-          c(apply(coefs, 1, max)), rev(apply(coefs, 1, min)),
+polygon(c(mineralien, rev(mineralien)),
+          c(apply(coefs, 1, max), rev(apply(coefs, 1, min))),
           col = "steelblue", border = NA)
 
+coef.stats <- cbind(apply(coefs, 1, quantile, .025),
+                    apply(coefs, 1, quantile, .975))
+matplot(mineralien, coef.stats, type = "n",
+        xlab = "Mineralien",
+        ylab = "Regressionskoeffizienten")
+abline(h = 0, col = "grey")
+polygon(c(mineralien, rev(mineralien)),
+        c(coef.stats[, 1], rev(coef.stats[, 2])),
+        col = "pink", border = NA)
+lines(mineralien, c(coef(min.pcr)))
